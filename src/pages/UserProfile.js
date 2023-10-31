3de4a9503bfb3e9a4e024aa9543dd930ad03939e
Anna Raptidou Cole <acrrj123@gmail.com>
2023-10-31 10:17:13 -0500

    Final version

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,19 +1,15 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useOutletContext } from "react-router-dom";
 
 function UserProfile() {
-  const [user, setUser] = useState({});
   const params = useParams();
-  const userId = params.id;
+  const users = useOutletContext()
+  //console.log(params) params.id is a string, console typeof(params.id) to see. Params.id returns the id of the object that was clicked. 
+  //console.log(users)
 
-  useEffect(() =>{
-    fetch(`http://localhost:4000/users/${userId}`)
-    .then(r => r.json())
-    .then(data => setUser(data))
-    .catch(error => console.error(error));
-  }, [userId]);
+  const user = users.find(user => user.id === parseInt(params.id))
+  //console.log(user) find return the element itself, as opposed to filter that returns an array of elements
 
-  if(!user.name){
+  if(!user){
     return <h1>Loading...</h1>;
   };
 
@@ -23,5 +19,7 @@ function UserProfile() {
       </aside>
   );
 };
+
+//Note: We're using an aside here instead of main because UserProfile is now being rendered as a child of Home, and Home already has a main element. HTML best practices dictate that there should be only one main element per page view. And, since UserProfile only appears on a nested route, we're displaying it in an aside, as it will appear alongside the list of users we're rendering. We can also use an empty tag instead <></>
 
 export default UserProfile;
